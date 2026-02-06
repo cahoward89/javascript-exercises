b4de16920350d3a899ca379d937a794534130782
cahoward89 <124751864+cahoward89@users.noreply.github.com>
2026-02-06 16:04:46 -0500

    Code to complete assignment

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC= (tempF-32)*(5/9);
+  return roundToOne(tempC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF=(tempC*(9/5))+32;
+  return roundToOne(tempF);
+};
+
+function roundToOne(num){
+  return Math.round(num*10)/10
 };
 
 // Do not edit below this line
