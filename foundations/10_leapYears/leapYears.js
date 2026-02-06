91b3df45655c66a3747377f045c5120f93f036eb
cahoward89 <124751864+cahoward89@users.noreply.github.com>
2026-02-06 11:30:38 -0500

    Code to complete assignment

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (Number.isInteger(num)==false) {return "Please enter a valid year"}
+    if (((num%4==0)&&(num%100!=0))||(num%400==0)) {return true}
+    return false
 };
 
 // Do not edit below this line
