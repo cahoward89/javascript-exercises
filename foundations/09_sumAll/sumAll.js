54e1f989838a0acd9e4493a0e4398c91a4649484
cahoward89 <124751864+cahoward89@users.noreply.github.com>
2026-02-05 19:31:24 -0500

    Code to complete assignment

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ((a<0)||(b<0)) {return "ERROR"};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {return "ERROR"}
+    if (a < 0 || b < 0) return "ERROR"
+
+    if (a>b){
+        [a,b]=[b,a]
+    };
+    let array=[]
+    for (let i=a;i<=b;i++){
+        array.push(i);
+        }
+    return array.reduce((acc,current)=>(acc+current),0);
 
 };
 
